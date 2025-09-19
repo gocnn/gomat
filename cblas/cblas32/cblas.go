a1ddd3a390157bb688384d7f04c6767f2202544f
CtrlX <gitctrlx@gmail.com>
2025-09-19 21:30:26 +0800

    fix: fix linux cgo link

--- cblas/cblas32/cblas.go
+++ cblas/cblas32/cblas.go
@@ -2,7 +2,7 @@ package cblas32
 
 /*
 #cgo CFLAGS: -g -O2
-#cgo windows LDFLAGS: -LC:/PROGRA~2/Intel/oneAPI/mkl/latest/lib -lmkl_rt
+#cgo windows LDFLAGS: -LC:/openblas/lib -lopenblas
 #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu/openblas-pthread -lopenblas
 #cgo darwin LDFLAGS: -framework Accelerate
 */
--- cblas/cblas64/cblas.go
+++ cblas/cblas64/cblas.go
@@ -3,7 +3,11 @@ package cblas64
 /*
 #cgo CFLAGS: -g -O2
 #cgo windows LDFLAGS: -LC:/PROGRA~2/Intel/oneAPI/mkl/latest/lib -lmkl_rt
+#cgo windows LDFLAGS: -LC:/openblas/lib -lopenblas
+#cgo linux LDFLAGS: -L/opt/intel/oneapi/mkl/latest/lib/intel64 -lmkl_rt
 #cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu/openblas-pthread -lopenblas
+#cgo linux LDFLAGS: -L/usr/lib/x86_64-linux-gnu -lopenblas
 #cgo darwin LDFLAGS: -framework Accelerate
+#cgo darwin LDFLAGS: -L/opt/homebrew/opt/openblas -lopenblas
 */
 import "C"
