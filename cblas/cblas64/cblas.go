465a6fb1a38bf1cdf1c6003fee0861d4d5abc2c4
CtrlX <gitctrlx@gmail.com>
2025-09-19 22:06:18 +0800

    feat: Update link

--- cblas/cblas64/cblas.go
+++ cblas/cblas64/cblas.go
@@ -3,8 +3,6 @@ package cblas64
 /*
 #cgo CFLAGS: -g -O2
 #cgo windows LDFLAGS: -LC:/PROGRA~2/Intel/oneAPI/mkl/latest/lib -lmkl_rt
-#cgo windows LDFLAGS: -LC:/openblas/lib -lopenblas
-
 #cgo linux LDFLAGS: -lopenblas
 #cgo darwin LDFLAGS: -framework Accelerate
 */
